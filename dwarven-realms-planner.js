1e37251a028f4a7e63c2f42229d869afe1df9231
GD <gd@gd>
2024-03-14 01:03:39 +0100

    Add basic canvas displaying the backdrop

---
+++ dwarven-realms-planner.js
@@ -0,0 +1,16 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext("2d");
+
+loadAndDrawBackdrop();
+
+function loadAndDrawBackdrop() {
+    const backdrop = new Image();
+    backdrop.onload = drawBackground;
+    backdrop.src = './resources/tree_blank.png';
+}
+
+function drawBackground() {
+    canvas.width = this.naturalWidth;
+    canvas.height = this.naturalHeight;
+    ctx.drawImage(this, 0, 0);
+}
