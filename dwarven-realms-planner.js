5e14029ba8e9bab86799702a12acd2e76829d6bd
GD <gd@gd>
2024-03-14 01:13:49 +0100

    Pass canvas and context as parameters

--- dwarven-realms-planner.js
+++ dwarven-realms-planner.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.querySelector('canvas');
     const ctx = canvas.getContext("2d");
 
-    loadAndDrawBackdrop();
+    loadAndDrawBackdrop(canvas, ctx);
 });
 
-function loadAndDrawBackdrop() {
+function loadAndDrawBackdrop(canvas, ctx) {
     const backdrop = new Image();
-    backdrop.onload = drawBackground;
+    backdrop.onload = image => drawBackground(image, canvas, ctx);
     backdrop.src = './resources/tree_blank.png';
 }
 
-function drawBackground() {
-    canvas.width = this.naturalWidth;
-    canvas.height = this.naturalHeight;
-    ctx.drawImage(this, 0, 0);
+function drawBackground(image, canvas, ctx) {
+    canvas.width = image.naturalWidth;
+    canvas.height = image.naturalHeight;
+    ctx.drawImage(image, 0, 0);
 }
