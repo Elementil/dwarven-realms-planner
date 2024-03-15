395c982495e70ad3b74af71bf94854b95970162d
GD <gd@gd>
2024-03-15 12:11:21 +0100

    Add JSDoc for IDE type checks

--- javascript/dwarven-realms-planner.js
+++ javascript/dwarven-realms-planner.js
@@ -1,8 +1,45 @@
-import node_types from '../data/node_types.json' assert { type: 'json' };
-import nodes from '../data/nodes.json' assert { type: 'json' };
+/**
+ * @typedef {object} Node
+ * @property {number} id
+ * @property {string} type
+ * @property {boolean?} rootNode
+ * @property {boolean?} active
+ * @property {number[]} neighbours
+ * @property {number} x
+ * @property {number} y
+ * @property {SVGCircleElement?} circle
+ * @property {SVGImageElement?} image
+ */
 
+/**
+ * @typedef {object} NodeEffect
+ * @property {string} type
+ * @property {number} value
+ * @property {string} unit
+ */
+
+/**
+ * @typedef {object} NodeType
+ * @property {string} label
+ * @property {string} image
+ * @property {string} description
+ * @property {NodeEffect[]} effects
+ */
+
+import nodeTypesImport from '../data/node_types.json' assert { type: 'json' };
+import nodesImport from '../data/nodes.json' assert { type: 'json' };
+
+/** @type Map<string, NodeType> */
+const nodeTypes = new Map(Object.entries(nodeTypesImport));
+
+/** @type Array<Node> */
+const nodes = nodesImport;
+
+/** @type number */
 let playerLevel = 1;
-let availablePoints;
+
+/** @type number */
+let availablePoints = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
     const planner = document.getElementById('planner');
@@ -14,6 +51,10 @@ document.addEventListener("DOMContentLoaded", () => {
     initResetButton();
 });
 
+/**
+ * 
+ * @returns {void}
+ */
 function initNodes() {
     const planner = document.getElementById('planner');
     nodes.forEach(node => {
@@ -30,12 +71,20 @@ function initNodes() {
     });
 }
 
+/**
+ * 
+ * @returns {void}
+ */
 function updateAvailablePoints() {
     const availablePointsText = document.getElementById('planner').getElementById('available-points');
     availablePoints = Math.min(4 + +playerLevel, 204) - nodes.filter(n => n.active).length;
     availablePointsText.textContent = availablePoints;
 }
 
+/**
+ * 
+ * @returns {void}
+ */
 function initPlayerLevelInput() {
     const playerLevelInput = document.getElementById('player-level');
     playerLevelInput.addEventListener('keyup', event => {
@@ -52,11 +101,20 @@ function initPlayerLevelInput() {
     });
 }
 
+/**
+ * 
+ * @returns {void}
+ */
 function initResetButton() {
     const resetButton = document.querySelector('button.reset-all');
     resetButton.addEventListener('click', () => resetAll());
 }
 
+/**
+ * 
+ * @param {Node} node 
+ * @returns {SVGCircleElement}
+ */
 function createNodeCircle(node) {
     const circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
     circle.setAttribute("r", "18.5px");
@@ -67,28 +125,47 @@ function createNodeCircle(node) {
     return circle;
 }
 
+/**
+ * 
+ * @param {Node} node 
+ * @returns {SVGImageElement}
+ */
 function createNodeImage(node) {
     const image = document.createElementNS("http://www.w3.org/2000/svg", 'image');
     image.setAttribute("x", node.x - 18.5);
     image.setAttribute("y", node.y - 18.5);
-    image.setAttribute("href", node_types[node.type].image);
+    image.setAttribute("href", nodeTypes.get(node.type).image);
     image.style.display = 'none';
     image.style.cursor = 'pointer';
     return image;
 }
 
+/**
+ * 
+ * @param {Node} node 
+ */
 function onNodeClicked(node) {
-    if (nodeStateChangable(node)) {
+    if (isNodeStateChangable(node)) {
         setNodeActive(node, !node.active);
         updateAvailablePoints();
     }
 }
 
+/**
+ * 
+ * @returns {void}
+ */
 function resetAll() {
     nodes.forEach(n => setNodeActive(n, false));
     updateAvailablePoints();
 }
 
+/**
+ * 
+ * @param {Node} node 
+ * @param {boolean} active 
+ * @returns {void}
+ */
 function setNodeActive(node, active) {
     if (active) {
         if (availablePoints < 1) {
@@ -103,24 +180,52 @@ function setNodeActive(node, active) {
     node.active = active;
 }
 
-function nodeStateChangable(node) {
+/**
+ * 
+ * @param {Node} node 
+ * @returns {boolean}
+ */
+function isNodeStateChangable(node) {
     const neighbours = getNeighbours(nodes, node);
-    return (node.rootNode || getActiveNeighbours(node, neighbours).length > 0) && (!node.active || !(hasDependentNeighbour(node, neighbours) || requiredNode(node)));
+    return (node.rootNode || getActiveNeighbours(node, neighbours).length > 0) && (!node.active || !(hasDependentNeighbour(node, neighbours) || isRequiredNode(node)));
 }
 
+/**
+ * 
+ * @param {Node[]} allNodes 
+ * @param {Node} node 
+ * @returns {Node[]}
+ */
 function getNeighbours(allNodes, node) {
     return allNodes.filter(n => node.neighbours.includes(n.id));
 }
 
+/**
+ * 
+ * @param {Node} node 
+ * @param {Node[]} neighbours 
+ * @returns {Node[]}
+ */
 function getActiveNeighbours(node, neighbours) {
     return neighbours.filter(n => n.active && n.neighbours.includes(node.id));
 }
 
+/**
+ * 
+ * @param {Node} node 
+ * @param {Node[]} neighbours 
+ * @returns {boolean}
+ */
 function hasDependentNeighbour(node, neighbours) {
     return !!neighbours.find(nb => nb.active && (!getNeighbours(nodes, nb).find(nbnb => nbnb.id !== node.id && nbnb.active && !node.rootNode) && !nb.rootNode));
 }
 
-function requiredNode(node) {
+/**
+ * 
+ * @param {Node} node 
+ * @returns {boolean}
+ */
+function isRequiredNode(node) {
     const nodesCopy = nodes.map(n => ({ id: n.id, neighbours: [...n.neighbours], rootNode: n.rootNode, active: n.active }));
     const simulatedNode = nodesCopy.find(n => n.id === node.id);
     const neighbours = getNeighbours(nodesCopy, simulatedNode);
@@ -135,17 +240,34 @@ function requiredNode(node) {
     return false;
 }
 
+/**
+ * 
+ * @param {Node} node 
+ * @param {Node[]} rootNodes 
+ * @param {Node[]} allNodes 
+ * @returns {boolean}
+ */
 function hasConnectionToAnyRootNode(node, rootNodes, allNodes) {
     return !!rootNodes.find(rn => hasConnectionToRootNode(node, rn, allNodes));
 }
 
+/**
+ * 
+ * @param {Node} node 
+ * @param {Node} rootNode 
+ * @param {Node[]} allNodes 
+ * @returns {boolean}
+ */
 function hasConnectionToRootNode(node, rootNode, allNodes) {
     if (node.id === rootNode.id) {
         return true;
     }
 
     const toDoSet = [node];
+
+    /** @type number[] */
     const doneSet = [];
+
     while (toDoSet.length > 0) {
         const currentNode = toDoSet.splice(0, 1)[0];
         doneSet.push(currentNode.id);
